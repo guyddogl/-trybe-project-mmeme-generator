26dc2349198db9a2378f0c5764ba87eceab1cda4
Guyddo <guyddogl@gmail.com>
2022-05-27 16:27:02 -0300

    Requisito 2

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const inputText = document.getElementById('text-input');
 const memeText = document.getElementById('meme-text');
+const memeImg = document.getElementById('meme-image');
 
 function showMemeText() {
   memeText.innerText = inputText.value;
 }
 
 inputText.addEventListener('keyup', showMemeText);
+
+function showMemeImage(event) {
+  let src = URL.createObjectURL(event.target.files[0]);
+  memeImg.src = src;
+}

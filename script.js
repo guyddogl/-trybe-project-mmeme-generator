e9f46e1c5af89a2ee41c86cd267f67868327416c
Guyddo <guyddogl@gmail.com>
2022-05-27 18:10:01 -0300

    Requisito 6

--- script.js
+++ script.js
@@ -2,16 +2,41 @@ const inputText = document.getElementById('text-input');
 const imageUpload = document.getElementById('meme-insert');
 const memeImg = document.getElementById('meme-image');
 const memeText = document.getElementById('meme-text');
+const memeContainer = document.getElementById('meme-image-container');
 
+// Requisito 1
 function showMemeText() {
   memeText.innerText = inputText.value;
 }
 
 inputText.addEventListener('keyup', showMemeText);
 
-function showMemeImage(event) {
+// Requisito 2
+function showUploadImage(event) {
   const src = URL.createObjectURL(event.target.files[0]); // https://developer.mozilla.org/pt-BR/docs/Web/API/URL/createObjectURl // https://developer.mozilla.org/en-US/docs/Web/API/File/Using_files_from_web_applications#example.3a_using_object_urls_to_display_images
   memeImg.src = src;
 }
 
-imageUpload.addEventListener('input', showMemeImage);
+imageUpload.addEventListener('input', showUploadImage);
+
+// Requisito 6
+function setBorder(event) {
+  const typeBorder = event.target.id;
+  memeContainer.className = '';
+  if (typeBorder === 'fire') {
+    memeContainer.classList.add('fire');
+  } else if (typeBorder === 'water') {
+    memeContainer.classList.add('water');
+  } else {
+    memeContainer.classList.add('earth');
+  }
+
+}
+
+const fire = document.getElementById('fire');
+const water = document.getElementById('water');
+const earth = document.getElementById('earth');
+
+fire.addEventListener('click', setBorder);
+water.addEventListener('click', setBorder);
+earth.addEventListener('click', setBorder);

934c3f0640d3d05c794342f2f9431607d9aec800
Guyddo <guyddogl@gmail.com>
2022-05-27 16:49:13 -0300

    Correções de lint

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function showMemeText() {
 inputText.addEventListener('keyup', showMemeText);
 
 function showMemeImage(event) {
-  let src = URL.createObjectURL(event.target.files[0]); // https://developer.mozilla.org/pt-BR/docs/Web/API/URL/createObjectURl // https://developer.mozilla.org/en-US/docs/Web/API/File/Using_files_from_web_applications#example.3a_using_object_urls_to_display_images
+  const src = URL.createObjectURL(event.target.files[0]); // https://developer.mozilla.org/pt-BR/docs/Web/API/URL/createObjectURl // https://developer.mozilla.org/en-US/docs/Web/API/File/Using_files_from_web_applications#example.3a_using_object_urls_to_display_images
   memeImg.src = src;
 }
 

1665241233b8e8e71e3b0bd96533c02e50fe0f28
Guyddo <guyddogl@gmail.com>
2022-05-27 16:45:01 -0300

    Refatorando o requisito 2

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const inputText = document.getElementById('text-input');
-const memeText = document.getElementById('meme-text');
+const imageUpload = document.getElementById('meme-insert');
 const memeImg = document.getElementById('meme-image');
+const memeText = document.getElementById('meme-text');
 
 function showMemeText() {
   memeText.innerText = inputText.value;
@@ -9,6 +10,8 @@ function showMemeText() {
 inputText.addEventListener('keyup', showMemeText);
 
 function showMemeImage(event) {
-  let src = URL.createObjectURL(event.target.files[0]);
+  let src = URL.createObjectURL(event.target.files[0]); // https://developer.mozilla.org/pt-BR/docs/Web/API/URL/createObjectURl // https://developer.mozilla.org/en-US/docs/Web/API/File/Using_files_from_web_applications#example.3a_using_object_urls_to_display_images
   memeImg.src = src;
 }
+
+imageUpload.addEventListener('input', showMemeImage);
